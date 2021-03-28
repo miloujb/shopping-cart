466aca852d917378d6e8d6b63b3ab2c705aa411f
miloujb <emilybailey@Emilys-MacBook-Air.local>
2021-03-28 18:20:32 +0100

    started to get total

--- offerCalculator.js
+++ offerCalculator.js
@@ -1,6 +1,14 @@
 function getTotal(item, quantity) {
     let result = {
         offerTotal: 0,
-        
+        unmatched: quantity
     }
+
+    let specialOffer = offers[item];
+    if (specialOffer && quantity >= specialOffer.quantity) {
+
+    }
+    return result
 }
+
+let offers = {}
